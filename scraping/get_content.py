e0f922e7b4c0570ff3ddfd61109cc09876fc7a7d
WildClown <gtsa@cin.ufpe.br>
2020-09-20 16:34:37 -0300

    Changed the get_content to run as a list of links, in order to make easier to extract the data.

--- scraping/get_content.py
+++ scraping/get_content.py
@@ -1,14 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://myanimelist.net/anime/35849/Darling_in_the_FranXX?q=Darlin&cat=anime"
+url = ["https://myanimelist.net/anime/35849/Darling_in_the_FranXX?q=Darlin&cat=anime","https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood"]
 
 
-def get_content(url, content="Studios:"):
-    html = requests.get(url).content
+def get_content(url, content="Studios:"):#Url will be an object
+  dataReturn = []
+  for x in url:
+    html = requests.get(x).content
     soup = BeautifulSoup(html, features="html.parser")
     divStudio = soup.find('span', string=content).parent
     studios = divStudio.find_all('a')
-    return [s.contents[0] for s in studios]
+    aux = [s.contents[0] for s in studios]
+    dataReturn.append(aux)
+  return dataReturn
 
-# print(get_content(url))
+print(get_content(url))
