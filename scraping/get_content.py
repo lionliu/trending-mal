9136331405e1f743be3eaccd0318990acaf88992
WildClown <gtsa@cin.ufpe.br>
2020-09-20 16:44:51 -0300

    Changed get_content to wait 5 sec between each link, and added an try except.

--- scraping/get_content.py
+++ scraping/get_content.py
@@ -9,10 +9,14 @@ def get_content(url, content="Studios:"):#Url will be an object
   for x in url:
     html = requests.get(x).content
     soup = BeautifulSoup(html, features="html.parser")
-    divStudio = soup.find('span', string=content).parent
-    studios = divStudio.find_all('a')
-    aux = [s.contents[0] for s in studios]
-    dataReturn.append(aux)
+    try:
+        divStudio = soup.find('span', string=content).parent
+        studios = divStudio.find_all('a')
+        aux = [s.contents[0] for s in studios]
+        dataReturn.append(aux)
+    except:
+        dataReturn.append([])
+    time.sleep(5)
   return dataReturn
 
 print(get_content(url))
