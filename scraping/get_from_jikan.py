c7a915ace79ddcad584ed379458f6eb1b6e0972c
WildClown <gtsa@cin.ufpe.br>
2020-09-23 18:36:31 -0300

    Refactor get_from_jikan.

--- scraping/get_from_jikan.py
+++ scraping/get_from_jikan.py
@@ -3,58 +3,51 @@ import time
 from urllib.request import Request, urlopen
 from tqdm import tqdm
 
+def get_field(field,obj,dataReturn):
+    if field in obj:
+        dataReturn[field].append(obj[field])
+    else:
+        dataReturn[field].append(0)
+
+def get_multi_field(field,obj,dataReturn):
+    if field in obj:
+        dataReturn[field].append([s["name"] for s in obj[field]])
+    else:
+        dataReturn["studio"].append([])
+
 def get_content(animeId):
     dataReturn = {
-        "ranked" : [],
+        "mal_id" : [],
+        "title" : [],
+        "studios" : [],
+        "genres" : [],
+        "source" : [],
+        "rank" : [],
         "score" : [],
         "popularity" : [],
         "members" : [],
-        "studios" : [],
-        "source" : [],
-        "favorites" : [] 
+        "favorites" : [],
+        "episodes" : []
     }
     for uid in tqdm(animeId):
         try:
             request = Request('https://api.jikan.moe/v3/anime/' + str(uid))
             response_body = urlopen(request).read()
             obj = json.loads(response_body)
-            if "rank" in obj:
-                dataReturn["ranked"].append(obj["rank"])
-            else:
-                dataReturn["ranked"].append(0)
-            if "score" in obj:
-                dataReturn["score"].append(obj["score"])
-            else:
-                dataReturn["score"].append(0)
-            if "popularity" in obj:
-                dataReturn["popularity"].append(obj["popularity"])
-            else:
-                dataReturn["popularity"].append(0)
-            if "members" in obj:
-                dataReturn["members"].append(obj["members"])
-            else:
-                dataReturn["members"].append(0)
-            if "studios" in obj:
-                dataReturn["studios"].append([s["name"] for s in obj["studios"]])
-            else:
-                dataReturn["studio"].append([])
-            if "source" in obj:
-                dataReturn["source"].append(obj["source"])
-            else:
-                dataReturn["source"].append("")
-            if "favorites" in obj:
-                dataReturn["favorites"].append(obj["favorites"])
-            else:
-                dataReturn["favorites"].append(0)
+            dataReturn["mal_id"].append(uid)
+            get_field("title",obj,dataReturn)
+            get_multi_field("studios",obj,dataReturn)
+            get_multi_field("genres",obj,dataReturn)
+            get_field("source",obj,dataReturn)
+            get_field("rank",obj,dataReturn)
+            get_field("score",obj,dataReturn)
+            get_field("popularity",obj,dataReturn)
+            get_field("members",obj,dataReturn)
+            get_field("favorites",obj,dataReturn)
+            get_field("episodes",obj,dataReturn)
         except:
-            dataReturn["ranked"].append(0)
-            dataReturn["score"].append(0)
-            dataReturn["popularity"].append(0)
-            dataReturn["members"].append(0)
-            dataReturn["studios"].append([])
-            dataReturn["source"].append("")
-            dataReturn["favorites"].append(0)
+            pass
         time.sleep(4)
     return (dataReturn)
 
-print( get_content([28891,23273,34599,35849]) )
+print( get_content([28891,2265,34599,35849]) )
