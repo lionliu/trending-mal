56ce0d2fe5a3c17da34aa1ee3a42c7072a103ba2
Leao Liu <llm2@cin.ufpe.br>
2020-09-26 15:40:28 -0300

    Finishing collecting new data

--- scraping/get_updated_data.py
+++ scraping/get_updated_data.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 from get_from_jikan import get_content
 
-animeList = pd.read_csv("../data/anime/animes.csv")
-animeList = animeList[:10]
+animeList = pd.read_csv(r'./animes.csv')
+# animeList = animeList[9655:-1]
 newDataSet = get_content(animeList["uid"])
 newDataSet = pd.DataFrame(newDataSet)
-newDataSet.to_csv("../data/anime/newAnimes.csv", index=False)
+newDataSet.to_csv(r'./newAnimes.csv', index=False)
