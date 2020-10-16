ac923ff7d545bae8fcec9b3f8dc6abf7433c330a
Leao Liu <llm2@cin.ufpe.br>
2020-10-16 17:49:50 -0300

    Updated download link at download_anime.py

--- data/download_anime.py
+++ data/download_anime.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from urllib.request import urlopen
 from zipfile import ZipFile
-zipurl = 'http://cin.ufpe.br/~llm2/anime.zip'
+zipurl = 'http://cin.ufpe.br/~llm2/newAnime.zip'
 with urlopen(zipurl) as zipresp:
     with ZipFile(BytesIO(zipresp.read())) as zfile:
         zfile.extractall()
